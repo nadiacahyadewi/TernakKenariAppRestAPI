e5bb7831920a6b5a4e0e405b8961b701dead5859
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-06-06 21:51:29 +0700

    membuat bloc get_burung_tersedia

---
+++ lib/presentation/bloc/get_burung_tersedia_bloc.dart
@@ -0,0 +1,28 @@
+import 'package:bloc/bloc.dart';
+import 'package:canaryapp/data/model/response/burung_semua_tersedia_model.dart';
+import 'package:canaryapp/data/repository/get_all_burung_tersedia_repository.dart';
+import 'package:meta/meta.dart';
+
+part 'get_burung_tersedia_event.dart';
+part 'get_burung_tersedia_state.dart';
+
+class GetBurungTersediaBloc 
+extends Bloc<GetBurungTersediaEvent, GetBurungTersediaState> {
+  final GetAllBurungTersediaRepository getAllBurungTersediaRepository;
+
+  GetBurungTersediaBloc(this.getAllBurungTersediaRepository) : super(GetBurungTersediaInitial()) {
+    on<GetAllBurungTersediaEvent>(_getAllBurungTersedia);
+  }
+
+  Future<void> _getAllBurungTersedia(
+    GetAllBurungTersediaEvent event,
+    Emitter<GetBurungTersediaState> emit,
+  ) async {
+    emit(GetBurungTersediaLoading());
+    final result = await getAllBurungTersediaRepository.getAllBurungTersedia();
+    result.fold(
+      (error) => emit(GetBurungTersediaError(message: error)),
+      (burungTersedia) => emit(GetBurungTersediaLoaded(burungTersedia: burungTersedia)),
+    );
+  }
+}
