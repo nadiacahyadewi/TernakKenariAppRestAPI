a07a26b521f21cc5a9ef9591bbb4b31f556d10d4
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-06-06 21:45:43 +0700

    membuat login_bloc

--- lib/presentation/auth/bloc/login_event.dart
+++
@@ -1,9 +0,0 @@
-part of 'login_bloc.dart';
-
-sealed class LoginEvent {}
-
-class LoginRequested extends LoginEvent {
-  final LoginRequestModel requestModel;
-
-  LoginRequested({required this.requestModel});
-}
