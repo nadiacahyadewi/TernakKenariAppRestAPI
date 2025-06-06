2e7bbb785604a3e11a541fe08e89af9f0c66cd1e
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-06-06 17:14:53 +0700

    membuat file get_all_burung_tersedia_repository.dart

---
+++ lib/data/repository/get_all_burung_tersedia_repository.dart
@@ -0,0 +1,41 @@
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:canaryapp/data/model/response/burung_semua_tersedia_model.dart';
+import 'package:canaryapp/services/service_http_client.dart';
+import 'package:dartz/dartz.dart';
+
+
+class GetAllBurungTersediaRepository {
+  final ServiceHttpClient httpClient;
+  GetAllBurungTersediaRepository(this.httpClient);
+
+  Future<Either<String, BurungSemuaTersediaModel>>
+  getAllBurungTersedia() async {
+    try {
+      final response = await httpClient.get("burung-semua-tersedia");
+
+      if (response.statusCode == 200) {
+        final jsonResponse = json.decode(response.body);
+        final burungTersediaResponse = BurungSemuaTersediaModel.fromJson(
+          jsonResponse,
+        );
+        return Right(burungTersediaResponse);
+      } else {
+        final errorMessage = json.decode(response.body);
+        return Left(errorMessage['message'] ?? 'Unknown error occurred');
+      }
+    } catch (e) {
+      if (e is SocketException) {
+        return Left("No Internet connection");
+      } else if (e is HttpException) {
+        return Left("HTTP error: ${e.message}");
+      } else if (e is FormatException) {
+        return Left("Invalid response format");
+      } else {
+        return Left("An unexpected error occurred: $e");
+      }
+    }
+  }
+}
